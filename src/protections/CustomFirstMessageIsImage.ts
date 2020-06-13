e65892c383cb1c1c3a73da16ac11bb2de900e436
Randshot <randshot@norealm.xyz>
2020-06-13 14:18:32 +0200

    feat: generate permalink locally
    Signed-off-by: Randshot <randshot@norealm.xyz>

--- src/protections/CustomFirstMessageIsImage.ts
+++ src/protections/CustomFirstMessageIsImage.ts
@@ -16,7 +16,7 @@ limitations under the License.
 
 import { IProtection } from "./IProtection";
 import { Mjolnir } from "../Mjolnir";
-import { LogLevel, LogService } from "matrix-bot-sdk";
+import { LogLevel, LogService, Permalinks, UserID } from "matrix-bot-sdk";
 import { logMessage } from "../LogProxy";
 import config from "../config";
 import { isTrueJoinEvent } from "../utils";
@@ -39,7 +39,7 @@ export class CustomFirstMessageIsImage implements IProtection {
         if (event['type'] === 'm.room.member') {
             if (isTrueJoinEvent(event)) {
                 this.justJoined[roomId].push(event['state_key']);
-                LogService.info("CustomFirstMessageIsImage", `Tracking ${event['state_key']} in ${roomId} as just joined`);
+                LogService.info('CustomFirstMessageIsImage', `Tracking ${event['state_key']} in ${roomId} as just joined`);
             }
 
             return; // stop processing (membership event spam is another problem)
@@ -51,14 +51,18 @@ export class CustomFirstMessageIsImage implements IProtection {
             const formattedBody = content['formatted_body'] || '';
             const isMedia = msgtype === 'm.image' || msgtype === 'm.video' || formattedBody.toLowerCase().includes('<img');
             if (isMedia && this.justJoined[roomId].includes(event['sender'])) {
+                await logMessage(LogLevel.WARN, 'CustomFirstMessageIsImage', `Kicking ${event['sender']} in ${roomId} for posting an image/video as the first thing after joining (see below)`);
+
                 const file = content['file'] || {}
                 const fileUrl = file['url'] || 'NOT_FOUND'
-                const eventId = event['event_id'] || 'NOT_FOUND'
-                await logMessage(LogLevel.WARN, "CustomFirstMessageIsImage", `Kicking ${event['sender']} in ${roomId} for posting an image/video as the first thing after joining: https://matrix.to/#/${roomId}/${eventId}?via=nop.zone | ${fileUrl}`, [], true);
+                const eventId = event['event_id']
+                const viaServers = [(new UserID(await mjolnir.client.getUserId())).domain];
+                const eventPermalink = eventId ? Permalinks.forEvent(roomId, eventId, viaServers) : 'NOT_FOUND'
+                await logMessage(LogLevel.INFO, 'CustomFirstMessageIsImage', `Event Permalink: ${eventPermalink} | MXC Url: ${fileUrl}`, [], true)
                 if (!config.noop) {
                     await mjolnir.client.kickUser(event['sender'], roomId, "[automated] first message is image/video protection");
                 } else {
-                    await logMessage(LogLevel.WARN, "CustomFirstMessageIsImage", `Tried to kick ${event['sender']} in ${roomId} but Mjolnir is running in no-op mode`, roomId);
+                    await logMessage(LogLevel.WARN, 'CustomFirstMessageIsImage', `Tried to kick ${event['sender']} in ${roomId} but Mjolnir is running in no-op mode`, roomId);
                 }
 
                 //if (this.recentlyKicked.includes(event['sender'])) return; // already handled (will be redacted)
@@ -67,16 +71,16 @@ export class CustomFirstMessageIsImage implements IProtection {
 
                 // Redact the event
                 if (!config.noop) {
-                    await mjolnir.client.redactEvent(roomId, event['event_id'], "[automated] first message is image/media protection");
+                    await mjolnir.client.redactEvent(roomId, event['event_id'], '[automated] first message is image/media protection');
                 } else {
-                    await logMessage(LogLevel.WARN, "CustomFirstMessageIsImage", `Tried to redact ${event['event_id']} in ${roomId} but Mjolnir is running in no-op mode`, roomId);
+                    await logMessage(LogLevel.WARN, 'CustomFirstMessageIsImage', `Tried to redact ${event['event_id']} in ${roomId} but Mjolnir is running in no-op mode`, roomId);
                 }
             }
         }
 
         const idx = this.justJoined[roomId].indexOf(event['sender']);
         if (idx >= 0) {
-            LogService.info("CustomFirstMessageIsImage", `${event['sender']} is no longer considered suspect`);
+            LogService.info('CustomFirstMessageIsImage', `${event['sender']} is no longer considered suspect`);
             this.justJoined[roomId].splice(idx, 1);
         }
     }
